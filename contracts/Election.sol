e9cd81527d21b6cb2deb857198af51474621835a
Nishant Aklecha <nishant.aklecha@gmail.com>
2019-07-31 16:35:08 +0530

    Added election contract

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,21 +1,14 @@
 pragma solidity 0.4.24;
 
 contract Election {
-    // Model a Candidate
     struct Candidate {
         uint id;
         string name;
         uint voteCount;
     }
-    // Store accounts that have voted
     mapping(address => bool) public voters;
-    // Store Candidates
-    // Fetch Candidate
     mapping(uint => Candidate) public candidates;
-    // Store Candidates Count
     uint public candidatesCount;
-
-    // voted event
     event votedEvent (
         uint indexed _candidateId
     );
@@ -31,19 +24,14 @@ contract Election {
     }
 
     function vote (uint _candidateId) public {
-        // require that they haven't voted before
         require(!voters[msg.sender]);
 
-        // require a valid candidate
         require(_candidateId > 0 && _candidateId <= candidatesCount);
 
-        // record that voter has voted
         voters[msg.sender] = true;
 
-        // update candidate vote Count
         candidates[_candidateId].voteCount ++;
 
-        // trigger voted event
         emit votedEvent(_candidateId);
     }
 }
