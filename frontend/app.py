fbc04d35eae80c4fb43ad74c27d6afb95ea8e3db
Nishant Aklecha <nishant.aklecha@gmail.com>
2019-08-09 01:20:49 +0530

    Added biometric verification function

--- frontend/app.py
+++ frontend/app.py
@@ -1,35 +1,15 @@
 from flask import Flask, render_template, flash, request
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 
-# App config.
-DEBUG = True
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
-class ReusableForm(Form):
-    name = TextField('Name:', validators=[validators.required()])
-    email = TextField('Email:', validators=[validators.required(), validators.Length(min=6, max=35)])
-    password = TextField('Password:', validators=[validators.required(), validators.Length(min=3, max=35)])
-    
-    @app.route("/", methods=['GET', 'POST'])
-    def hello():
-        form = ReusableForm(request.form)
-    
-        print(form.errors)
-        if request.method == 'POST':
-            name=request.form['name']
-            password=request.form['password']
-            email=request.form['email']
-            print(name, " ", email, " ", password)
-    
-        if form.validate():
-        # Save the comment here.
-            flash('Thanks for registration ' + name)
-        else:
-            flash('Error: All the form fields are required. ')
-    
-        return render_template('hello.html', form=form)
+@app.route("/", methods=['GET', 'POST'])
+def hello():
+    if request.method == 'POST':
+        name = request.form['name']
+        bio = request.form['biometric']
+        print(name,bio)
+    return render_template('verification.html')
 
 if __name__ == '__main__':
 	app.run(host="127.0.0.1" ,port=4000, debug = True)
