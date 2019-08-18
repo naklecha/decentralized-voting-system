6e7d8dd9ee5d311feac52915857bacad416014e0
Nishant Aklecha <nishant.aklecha@gmail.com>
2019-08-18 19:09:46 +0530

    updated india stack simulation

--- backend.py
+++ backend.py
@@ -22,9 +22,31 @@ contract_addr = tx_receipt.contractAddress
 app = Flask(__name__)
 app.secret_key = 'i love white chocolate too'
 
-accounts = eval(open('accounts').read())
+accounts = [ 
+    '0xB0BE5EFDe83490f0d8fC64120461660098AE7599',
+    '0xe0e577218063fc8648A4b04fDcFe2fD02990f734',
+    '0xA55a74F93a50529f6F85658C22538eD9035551c3',
+    '0x0b918ccbA35F62d80530F0223e0eA0DaB0C879BA',
+    '0xCfc9EA020080496D2162B6EE8BFC84B5fbDF1FB5',
+    '0xd84A6127e81Eb9384eB30b6A2D54E29A91798651',
+    '0x35A3c8A48fb58B1d418D62c752Eeb4DA36128564',
+    '0x50EC809051d823D0F979a1a974Ad63568bf7BEC1',
+    '0x56Df378eF6AEB25Ec4fa22eDdB8f88B8407C2334',
+    '0xa5cc1434694afA6daB030EEa5D6e4235e405643E' 
+]
 
-privatekeys = eval(open('private_keys').read())
+privatekeys = [
+    '25d9479cd21fb800522f8e0c74513f0730f7afac9f3ac7a23d8ad69b7103be52',
+    '01519292e7b9fb0d98149b7b202cbd2b99d92857804140fe927855cc26026a9d',
+    '34f4f6e8a79cb192b957965b7764eb949f588027aee590557160bd2eca64123c',
+    'cc85e4484592eadf0681fe9f2d6297c1cf4cb81321fe9454b43ad04bedcab87a',
+    '4997b54bc7b835335f4324ef4e9f27fc78e42068799c07dba7f2ee517e995808',
+    '448f522922fcfd184712c8b7e8bb82de255c6ca6aa8dda76d1e7aa4bdb819d49',
+    '571f96e9291b198b7e024ab950d5868dbdc6b6137ee30bddb51ff450fa7591ce',
+    '1e4acf03770503c9e5487fab22bb81484968a50b293d6bf528eb9e0fd5bd542e',
+    '7c7aacbc4b3c839e0e870d6c08f63e3816c0b1384aa1dc6d05e9de95f2589106',
+    '58f129cbace24078382559c366c6427f88179d35f2fac2ccdf62591e7e59cbac'
+]
 
 vote_tx = []
 voted = []
