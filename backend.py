5cd424464e63b383f6e0277abbfb6b3cbabce324
Nishant Aklecha <nishant.aklecha@gmail.com>
2019-08-18 18:36:36 +0530

    Fixed path to 0.0.0.0

--- backend.py
+++ backend.py
@@ -94,4 +94,4 @@ def candidates_list():
         return "Error processing",500
 
 if __name__ == '__main__':
-	app.run(host="127.0.0.1" ,port=3000, debug = True)
+	app.run(host="0.0.0.0" ,port=80, debug = True)
